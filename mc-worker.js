b643c4eff3896faeb854ae58c44fe86af4df0c52
Avaer Kazmer <a@modules.io>
2019-07-30 19:05:27 -0400

    Use new allocator in mc-worker

--- mc-worker.js
+++ mc-worker.js
@@ -63,36 +63,24 @@ const _handleMessage = data => {
   const {method} = data;
   switch (method) {
     case 'march': {
-      const offsets = [];
-      const _alloc = (constructor, size) => {
-        const offset = self.LocalModule._doMalloc(size * constructor.BYTES_PER_ELEMENT);
-        const b = new constructor(self.LocalModule.HEAP8.buffer, self.LocalModule.HEAP8.byteOffset + offset, size);
-        b.offset = offset;
-        offsets.push(offset);
-        return b;
-      };
-      const _freeAll = () => {
-        for (let i = 0; i < offsets.length; i++) {
-          self.LocalModule._doFree(offsets[i]);
-        }
-      };
+      const allocator = new Allocator();
 
       const {dims: dimsData, potential: potentialData, shift: shiftData} = data;
-      const dims = _alloc(Int32Array, 3);
+      const dims = allocator.alloc(Int32Array, 3);
       dims[0] = dimsData[0];
       dims[1] = dimsData[1];
       dims[2] = dimsData[2];
-      const potential = _alloc(Float32Array, potentialData.length);
+      const potential = allocator.alloc(Float32Array, potentialData.length);
       potential.set(potentialData);
-      const shift = _alloc(Float32Array, 3);
+      const shift = allocator.alloc(Float32Array, 3);
       shift[0] = shiftData[0];
       shift[1] = shiftData[1];
       shift[2] = shiftData[2];
       const indexOffset = 0;
-      const positions = _alloc(Float32Array, 1024*1024/Float32Array.BYTES_PER_ELEMENT);
-      const faces = _alloc(Uint32Array, 1024*1024/Uint32Array.BYTES_PER_ELEMENT);
-      const positionIndex = _alloc(Uint32Array, 1);
-      const faceIndex = _alloc(Uint32Array, 1);
+      const positions = allocator.alloc(Float32Array, 1024*1024/Float32Array.BYTES_PER_ELEMENT);
+      const faces = allocator.alloc(Uint32Array, 1024*1024/Uint32Array.BYTES_PER_ELEMENT);
+      const positionIndex = allocator.alloc(Uint32Array, 1);
+      const faceIndex = allocator.alloc(Uint32Array, 1);
       self.LocalModule._doMarchingCubes(
         dims.offset,
         potential.offset,
