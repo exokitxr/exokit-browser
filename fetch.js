794a6c8d62caff36ebbc87d4cdd4740431fffb51
Avaer Kazmer <a@modules.io>
2019-08-21 05:03:09 -0400

    Add fetch.js proxy fetching support

---
+++ fetch.js
@@ -0,0 +1,10 @@
+window.fetch = (_fetch => function fetch(u, init) {
+  if (!/^https?:\/\//.test(u) || /^https:\/\/(?:.+?\.)?webaverse.com$/.test(u)) {
+    // nothing
+  } else {
+    const oldUrl = u;
+    const parsedUrl = new URL(u);
+    u = 'https://' + parsedUrl.origin.replace(/^(https):\/\//, '$1-').replace(/:([0-9]+)$/, '-($1)').replace(/\./g, '-') + '.proxy.webaverse.com' + parsedUrl.pathname;
+  }
+  return _fetch(u, init);
+})(window.fetch);
