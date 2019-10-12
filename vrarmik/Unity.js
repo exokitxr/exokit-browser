c75ebdf1c373819ccbf005bd7d3f33d232ef2e50
Avaer Kazmer <a@modules.io>
2019-10-11 23:47:50 -0400

    Add Unity math library

--- vrarmik/Unity.js
+++ vrarmik/Unity.js
@@ -63,6 +63,65 @@ class Transform {
   }
 }
 
+const Mathf = {
+  Deg2Rad: DEG2RAD,
+  Rad2Deg: RAD2DEG,
+  PI: Math.PI,
+  Clamp(v, min, max) {
+    return Math.min(Math.max(v, min), max);
+  },
+  Clamp01(v) {
+    return Mathf.Clamp(v, 0, 1);
+  },
+  Min(a, b) {
+    return Math.min(a, b);
+  },
+  Max(a, b) {
+    return Math.max(a, b);
+  },
+  Abs(v) {
+    return Math.abs(v);
+  },
+  Lerp(a, b, t) {
+    return a*(1-v) + b*v;
+  },
+  LerpAngle(a, b, t) {
+    const num = Mathf.Repeat(b - a, 360);
+    if (num > 180) {
+      num -= 360;
+    }
+    return a + num * Mathf.Clamp01(t);
+  },
+  Floor(v) {
+    return Math.floor(v);
+  },
+  Ceil(v) {
+    return Math.ceil(v);
+  },
+  Repeat(t, length) {
+    return t - Mathf.Floor(t / length) * length;
+  },
+  DeltaAngle(current, target) {
+    let num = Mathf.Repeat(target - current, 360);
+    if (num > 180) {
+      num -= 360;
+    }
+    return num;
+  },
+  Acos(v) {
+    return Math.acos(v);
+  },
+  Atan2(a, b) {
+    return Math.atan2(a, b);
+  },
+  Sign(v) {
+    return v >= 0 ? 1 : -1;
+  },
+  Pow(a, b) {
+    return Math.pow(a, b);
+  },
+};
+
 const PlayerPrefs = {
   data: {},
   GetFloat(k, d) {
@@ -85,6 +144,7 @@ export {
   Vector3,
   Quaternion,
   Transform,
+  Mathf,
   PlayerPrefs,
   XRSettings,
 };
