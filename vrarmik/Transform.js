07b8baf3300c09c788a71a2922709313150bea7d
Avaer Kazmer <a@modules.io>
2019-10-11 20:45:47 -0400

    Add initial Transform.js

---
+++ vrarmik/Transform.js
@@ -0,0 +1,9 @@
+class Transform {
+  constructor() {
+    this.position = new Vector3();
+    this.rotation = new Quaternion();
+    this.scale = new Vector3(1, 1, 1);
+  }
+}
+
+export default Transform;
