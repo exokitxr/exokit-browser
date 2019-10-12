760b63bc6ec387920e9dd88cb97c91a1e09876c4
Avaer Kazmer <a@modules.io>
2019-10-11 23:51:14 -0400

    Port VectorHelpers

--- vrarmik/Utils/VectorHelpers.js
+++ vrarmik/Utils/VectorHelpers.js
@@ -1,14 +1,16 @@
+import {Vector3, Mathf} from '../Unity.js';
+
 const VectorHelpers = {
-	axisAngle(Vector3 v, Vector3 forward, Vector3 axis)
+	axisAngle(v, forward, axis)
 	{
-		Vector3 right = Vector3.Cross(axis, forward);
+		const right = Vector3.Cross(axis, forward);
 		forward = Vector3.Cross(right, axis);
 		return Mathf.Atan2(Vector3.Dot(v, right), Vector3.Dot(v, forward)) * Mathf.Rad2Deg;
 	},
-  getAngleBetween(Vector3 a, Vector3 b, Vector3 forward, Vector3 axis)
+  getAngleBetween(a, b, forward, axis)
 	{
-		float angleA = axisAngle(a, forward, axis);
-		float angleB = axisAngle(b, forward, axis);
+		const angleA = this.axisAngle(a, forward, axis);
+		const angleB = this.axisAngle(b, forward, axis);
 
 		return Mathf.DeltaAngle(angleA, angleB);
 	},
