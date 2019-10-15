abadabed98261a9834eb055083f2a16e205c7c69
Avaer Kazmer <a@modules.io>
2019-10-15 00:06:15 -0400

    Bugfix shoulder transforms

--- vrarmik/ShoulderTransforms.js
+++ vrarmik/ShoulderTransforms.js
@@ -11,33 +11,46 @@ class ShoulderTransforms extends MonoBehavior
       super(...args);
 
 			this.leftShoulder = new Transform();
+			this.transform.AddChild(this.leftShoulder);
 
 			this.rightShoulder = new Transform();
+			this.transform.AddChild(this.rightShoulder);
 			/* this.leftShoulderRenderer = new Transform();
 			this.rightShoulderRenderer = new Transform(); */
 			this.leftShoulderAnchor = new Transform();
+			this.leftShoulderAnchor.localPosition = new Vector3(-0.3, 0, 0);
+			this.transform.AddChild(this.leftShoulderAnchor);
 			this.rightShoulderAnchor = new Transform();
+			this.rightShoulderAnchor.localPosition = new Vector3(0.3, 0, 0);
+			this.transform.AddChild(this.rightShoulderAnchor);
 			this.leftArm = null;
 			this.rightArm = null;
 		}
 
 		OnEnable()
 		{
+			const shoulderPoser = this.GetComponent(ShoulderPoser);
 			if (this.leftArm === null)
 			{
 				this.leftArm = new GameObject().AddComponent(ArmTransforms);
 				const armIk = this.leftArm.GetComponentInChildren(VRArmIK);
 				armIk.shoulder = this;
-				armIk.shoulderPoser = this.GetComponent(ShoulderPoser);
+				armIk.shoulderPoser = shoulderPoser;
 				armIk.target = armIk.shoulderPoser.avatarTrackingReferences.leftHand.transform;
+				this.leftShoulderAnchor.AddChild(this.leftArm.transform);
 			}
 			if (this.rightArm === null)
 			{
 				this.rightArm = new GameObject().AddComponent(ArmTransforms);
 				const armIk = this.rightArm.GetComponentInChildren(VRArmIK);
 				armIk.shoulder = this;
-				armIk.shoulderPoser = this.GetComponent(ShoulderPoser);
+				armIk.shoulderPoser = shoulderPoser;
 				armIk.target = armIk.shoulderPoser.avatarTrackingReferences.rightHand.transform;
+
+				armIk.left = false;
+        this.rightArm.lowerArm.localPosition = new Vector3(0.3, 0, 0);
+        this.rightArm.hand.localPosition = new Vector3(0.3, 0, 0);
+        this.rightShoulderAnchor.AddChild(this.rightArm.transform);
 			}
 		}
 
