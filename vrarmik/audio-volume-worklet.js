76b52be62a5db137d72a46c2b0d8804c2e0a9db3
Avaer Kazmer <a@modules.io>
2019-10-28 21:01:10 -0400

    Add audo volume worklet muted message support

--- vrarmik/audio-volume-worklet.js
+++ vrarmik/audio-volume-worklet.js
@@ -3,8 +3,22 @@ const numTicks = 1;
 let tick = 0;
 let sampleSum = 0;
 let numSamples = 0;
+let muted = true;
 
 class VolumeProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+
+    this.port.addEventListener('message', e => {
+      const data = JSON.parse(e.data);
+      console.log('got worklet data', e.data);
+      const {method} = data;
+      if (method === 'muted') {
+        muted = data.muted;
+      }
+    });
+    this.port.start();
+  }
   process(inputs, outputs) {
     const channels = inputs[0];
     // const output = outputs[0];
@@ -26,17 +40,18 @@ class VolumeProcessor extends AudioWorkletProcessor {
       numSamples = 0;
     }
 
-    for (let i = 0; i < outputs.length; i++) {
-      const input = inputs[i];
-      const output = outputs[i];
+    if (!muted) {
+      for (let i = 0; i < outputs.length; i++) {
+        const input = inputs[i];
+        const output = outputs[i];
 
-      for (let channel = 0; channel < output.length; channel++) {
-        output[channel].set(input[channel]);
+        for (let channel = 0; channel < output.length; channel++) {
+          output[channel].set(input[channel]);
+        }
       }
     }
 
     return true;
   }
 }
-
 registerProcessor('volume-processor', VolumeProcessor);
