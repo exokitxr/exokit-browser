ee09e3b5ab5bdd98220be9a6243c6fea33d5a5eb
Avaer Kazmer <a@modules.io>
2019-10-30 00:51:42 -0400

    Initialize finger gamepad input values

--- vrarmik/VRTrackingReferences.js
+++ vrarmik/VRTrackingReferences.js
@@ -9,7 +9,11 @@ import {Transform} from './Unity.js';
         console.log('change 1', new Error().stack);
       }; */
       this.leftHand = new Transform();
+      this.leftHand.pointer = 0;
+      this.leftHand.grip = 0;
       this.rightHand = new Transform();
+      this.rightHand.pointer = 0;
+      this.rightHand.grip = 0;
       this.head = new Transform();
       /* this.head.onchange = () => {
         console.log('change 2', new Error().stack);
