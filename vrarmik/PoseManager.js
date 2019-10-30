e28be2acaf5d09581220667dbb8eb70b7793cccf
Avaer Kazmer <a@modules.io>
2019-10-30 04:58:23 -0400

    More pose manager cleanup

--- vrarmik/PoseManager.js
+++ vrarmik/PoseManager.js
@@ -13,7 +13,7 @@ class PoseManager
 
       // Oculus uses a different reference position -> 0 is the reference head position if the user is standing in the middle of the room. 
       // In OpenVR, the 0 position is the ground position and the user is then at (0, playerHeightHmd, 0) if he is in the middle of the room, so I need to correct this for shoulder calculation 
-      this.vrSystemOffsetHeight = 0.0;
+      // this.vrSystemOffsetHeight = 0.0;
 
 			this.referencePlayerHeightHmd = 1.7;
 			this.referencePlayerWidthWrist = 1.39;
@@ -43,7 +43,7 @@ class PoseManager
       {
           this.loadPlayerSize();
       }
-      this.vrSystemOffsetHeight = 0;
+      // this.vrSystemOffsetHeight = 0;
     }
 
 		/* Start()
