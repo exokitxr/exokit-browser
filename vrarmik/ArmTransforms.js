db155c3c2a24b4da00f1b95efc2d06cd83e7f1eb
Avaer Kazmer <a@modules.io>
2019-11-01 20:44:37 -0400

    Remove unity Vector3/Quaternion

--- vrarmik/ArmTransforms.js
+++ vrarmik/ArmTransforms.js
@@ -1,5 +1,3 @@
-import {Vector3, Quaternion, Mathf} from './Unity.js';
-
 class ArmTransforms
 	{
 		constructor() {
--- vrarmik/Unity.js
+++ vrarmik/Unity.js
@@ -2,167 +2,6 @@ const DEG2RAD = Math.PI/180;
 const RAD2DEG = 180/Math.PI;
 const ORDER = 'ZXY';
 
-class Vector3 extends THREE.Vector3 {
-  bindOnchange(onchange) {
-    let x = this.x, y = this.y, z = this.z;
-    Object.defineProperty(this, 'x', {
-      get() {
-        return x;
-      },
-      set(newX) {
-        x = newX;
-        onchange();
-      },
-    });
-    Object.defineProperty(this, 'y', {
-      get() {
-        return y;
-      },
-      set(newY) {
-        y = newY;
-        onchange();
-      },
-    });
-    Object.defineProperty(this, 'z', {
-      get() {
-        return z;
-      },
-      set(newZ) {
-        z = newZ;
-        onchange();
-      },
-    });
-    this.set = (_set => function set() {
-      _set.apply(this, arguments);
-      onchange();
-    })(this.set);
-    this.copy = (_copy => function copy() {
-      _copy.apply(this, arguments);
-      onchange();
-    })(this.copy);
-  }
-
-  static get zero() {
-    return new Vector3(0, 0, 0);
-  }
-  static get one() {
-    return new Vector3(1, 1, 1);
-  }
-  static get left() {
-    return new Vector3(-1, 0, 0);
-  }
-  static get right() {
-    return new Vector3(1, 0, 0);
-  }
-  static get up() {
-    return new Vector3(0, 1, 0);
-  }
-  static get down() {
-    return new Vector3(0, -1, 0);
-  }
-  static get forward() {
-    return new Vector3(0, 0, 1);
-  }
-  static get back() {
-    return new Vector3(0, 0, -1);
-  }
-  get magnitude() {
-    return this.length();
-  }
-  get normalized() {
-    return this.clone().normalize();
-  }
-  static Scale(a, b) {
-    return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
-  }
-  Scale(v) {
-    return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z);
-  }
-  static Dot(a, b) {
-    return a.dot(b);
-  }
-  static Cross(a, b) {
-    return a.clone().cross(b);
-  }
-  static Angle(a, b) {
-    return a.angleTo(b) * RAD2DEG;
-  }
-}
-
-class Quaternion extends THREE.Quaternion {
-  bindOnchange(onchange) {
-    let x = this.x, y = this.y, z = this.z, w = this.w;
-    Object.defineProperty(this, 'x', {
-      get() {
-        return x;
-      },
-      set(newX) {
-        x = newX;
-        onchange();
-      },
-    });
-    Object.defineProperty(this, 'y', {
-      get() {
-        return y;
-      },
-      set(newY) {
-        y = newY;
-        onchange();
-      },
-    });
-    Object.defineProperty(this, 'z', {
-      get() {
-        return z;
-      },
-      set(newZ) {
-        z = newZ;
-        onchange();
-      },
-    });
-    Object.defineProperty(this, 'w', {
-      get() {
-        return w;
-      },
-      set(newW) {
-        w = newW;
-        onchange();
-      },
-    });
-    this.set = (_set => function set() {
-      _set.apply(this, arguments);
-      onchange();
-    })(this.set);
-    this.copy = (_copy => function copy() {
-      _copy.apply(this, arguments);
-      onchange();
-    })(this.copy);
-    /* this.setFromAxisAngle = (_setFromAxisAngle => function setFromAxisAngle() {
-      _setFromAxisAngle.apply(this, arguments);
-      onchange();
-    })(this.setFromAxisAngle); */
-  }
-
-  static get identity() {
-    return new Quaternion(0, 0, 0, 1);
-  }
-  static AngleAxis(angle, axis) {
-    return new Quaternion().setFromAxisAngle(axis, angle * DEG2RAD);
-  }
-  static FromToRotation(a, b) {
-    return new Quaternion().setFromUnitVectors(a, b);
-  }
-  static Euler(v) {
-    return new Quaternion().setFromEuler(new THREE.Euler(v.x * DEG2RAD, v.y * DEG2RAD, v.z * DEG2RAD, ORDER));
-  }
-  static Inverse(q) {
-    return q.clone().inverse();
-  }
-
-  Inverse() {
-    return this.clone().inverse();
-  }
-}
-
 /* const Time = {
   deltaTime: 1/90,
 }; */
@@ -282,8 +121,8 @@ const Mathf = {
     }
   }
 } */
-const localVector = new Vector3();
-const localVector2 = new Vector3();
+const localVector = new THREE.Vector3();
+const localVector2 = new THREE.Vector3();
 const Helpers = {
   getWorldPosition(o, v) {
     return v.setFromMatrixPosition(o.matrixWorld);
@@ -308,8 +147,6 @@ const Helpers = {
 };
 
 export {
-  Vector3,
-  Quaternion,
   // Time,
   Mathf,
   // XRSettings,
