6b642bc7b60217912ac02829466196d17733b2e2
Avaer Kazmer <a@modules.io>
2019-11-08 01:45:33 -0500

    Small skeleton binding cleanup

--- vrarmik/avatars.js
+++ vrarmik/avatars.js
@@ -76,9 +76,9 @@ class Avatar {
 
     const skeletonSkinnedMesh = skinnedMeshes.find(o => o.skeleton.bones[0].parent) || null;
     const skeleton = skeletonSkinnedMesh && skeletonSkinnedMesh.skeleton;
-    if (skeleton) {
+    /* if (skeleton) {
       skeletonSkinnedMesh.bind(skeleton);
-    }
+    } */
     const poseSkeletonSkinnedMesh = skeleton ? skinnedMeshes.find(o => o.skeleton !== skeleton && o.skeleton.bones.length >= skeleton.bones.length) : null;
     const poseSkeleton = poseSkeletonSkinnedMesh && poseSkeletonSkinnedMesh.skeleton;
     if (poseSkeleton) {
